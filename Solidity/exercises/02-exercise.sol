0201e39021e128c9ab5ae18c8476f8046e98a92c
Miguel Paya <miguelpaya3.5@gmail.com>
2024-08-25 19:09:58 +0200

    adding info about visibility and events, adding a new exercise.

---
+++ Solidity/exercises/02-exercise.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+
+/*
+## Example Exercise
+
+**Build a smart contract simulating a Bank**
+
+### Variables
+
+- A list where we save the balance of an account with address and amount.
+
+### Functions
+
+- A function to add balance to their own account.
+- A function to return their own balance.
+- A function to transfer balance between accounts (address): 
+    - Inside function to transfer balance with 3 parámeters: the receiver, the sender, the amount.
+*/
+
+pragma solidity ^0.8.14;
