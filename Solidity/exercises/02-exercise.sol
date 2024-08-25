f6e5da5f01cfb1d28721c868954182511453ee15
Miguel Paya <miguelpaya3.5@gmail.com>
2024-08-25 19:46:18 +0200

    finished exercise about create and simulate the bank functionality

--- Solidity/exercises/02-exercise.sol
+++ Solidity/exercises/02-exercise.sol
@@ -17,4 +17,39 @@
     - Inside function to transfer balance with 3 parámeters: the receiver, the sender, the amount.
 */
 
-pragma solidity ^0.8.14;
+pragma solidity >=0.7.0;
+
+contract Bank {
+
+    struct BankAccount {
+        address owner;
+        uint256 balance;
+    }
+
+    mapping (address => BankAccount) private bank_accounts;
+
+    // Add balance to the caller's account
+    function addBalance (uint8 _amount) public {
+        bank_accounts[msg.sender].balance += _amount;
+    }
+
+    // Get the balance of the caller's account
+    function getBalance() public view returns (uint256) {
+        return bank_accounts[msg.sender].balance;
+    }
+
+    // Private function to handle balance transfer between accounts
+    function transferBalancePrivate(address _sender, address _receiver, uint256 _amount) private {
+        if (bank_accounts[_sender].balance < _amount) {
+            return;
+        } else {
+            bank_accounts[_sender].balance -= _amount;
+            bank_accounts[_receiver].balance += _amount;
+        }
+    }
+
+    // Public function to initiate a balance transfer
+    function transferBalance(address _receiver, uint256 _amount) public {
+        transferBalancePrivate(msg.sender, _receiver, _amount);
+    }
+}
