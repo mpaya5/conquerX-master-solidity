fe3c770459ad136a16b4e8184cc3c7c24e4fed9c
Miguel Paya <miguelpaya3.5@gmail.com>
2024-08-25 18:23:56 +0200

    finished exercise 01

--- Solidity/exercises/01-exercise.sol
+++ Solidity/exercises/01-exercise.sol
@@ -1,20 +1,48 @@
 // SPDX-License-Identifier: GPL-3.0
 
 /*
-## Ejercicio Ejemplo
+## Example Exercise
 
-**Construir un smart contract en el que unos estudiantes puedan registrarse en una lista:**
+**Build a smart contract where students can register on a list:**
 
 ### Variables
 
-- Una estructura de tipo `Estudiante`, con sus características propias.
-- Una lista en la que los estudiantes puedan registrarse con su dirección de Ethereum (`address`).
+- A `Student` structure, with its own characteristics.
+- A list where students can register using their Ethereum address (`address`).
 
-### Funciones
+### Functions
 
-- Una función para registrarse en la lista.
-- Una función para que cada estudiante pueda consultar sus datos.
-- Una función para ver la información de todos los estudiantes.
+- A function to register on the list.
+- A function for each student to view their own data.
+- A function to view the information of all students.
 */
 
 pragma solidity >=0.7.0;
+pragma experimental ABIEncoderV2;
+
+contract StudentsList {
+    // Represent a unique Student
+    struct Student {
+        address studentIdentifier;
+        string name;
+        uint8 age;
+    }
+    // Each Student is associated to an address
+    mapping (address => Student) private students;
+    Student [] allStudents;
+    // Create a new Student
+    function addStudent (string memory _name, uint8 _age) public returns (bool success)
+    {
+        students[msg.sender] = Student(msg.sender, _name, _age);
+        allStudents.push(students[msg.sender]);
+        return true;
+    }
+    // Get all the Student Data (just if the ones who execute the function is a student)
+    function getOwnData() public view returns (Student memory) {
+        return students[msg.sender];
+    }
+    // Get all the students data
+    function getAllData() public view returns (Student[] memory) {
+        return allStudents;
+    }
+}
