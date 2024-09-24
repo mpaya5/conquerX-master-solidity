117ded75044b9de989897c3faa8c82a1d98c56c4
Miguel Payá <miguelpaya3.5@gmail.com>
2024-09-24 19:33:34 +0200

    TODO: To fix the errors with the tests again

--- Solidity/test/NFTTest.js
+++ Solidity/test/NFTTest.js
@@ -1,4 +1,3 @@
-const { web3 } = require('@openzeppelin/test-helpers/src/setup');
 const { expect } = require('chai');
 
 const NFT = artifacts.require("NFT");
@@ -10,18 +9,16 @@ contract("NFT", (accounts) => {
         this.nftInstance = await NFT.new("MyNFTCollection", "MNFT", { from: owner });
     });
 
-    it('should create a new NFT with the correct details', async () => {
+    it('should create a new NFT with the correct details', async function () {
         await this.nftInstance.createRandomNFT("NFT1", { from: user1, value: web3.utils.toWei('2', 'ether') });
         await this.nftInstance.createRandomNFT("NFT2", { from: user2, value: web3.utils.toWei('2', 'ether') });
 
         const user1NFTs = await this.nftInstance.getNftsByOwner(user1);
         const user2NFTs = await this.nftInstance.getNftsByOwner(user2);
 
-        expect(user1NFTs.length).to.equal(1);
-        expect(user2NFTs.length).to.equal(1);
-        expect(user1NFTs[0].name).to.equal("NFT1");
-        expect(user2NFTs[0].name).to.equal("NFT2");
+        expect(user1NFTs.length.toString()).to.equal("1", "The number of NFTs owned by user1 is incorrect");
+        expect(user2NFTs.length.toString()).to.equal("1", "The number of NFTs owned by user2 is incorrect");
+        expect(user1NFTs[0].name).to.equal("NFT1", "The name of the NFT owned by user1 is incorrect");
+        expect(user2NFTs[0].name).to.equal("NFT2", "The name of the NFT owned by user2 is incorrect");
     });
-
-    
-})
+});
